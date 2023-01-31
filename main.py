cd0bc5e4662c89b1a6fcf2de14d7d66f023edb35
Kuba <kubadziekan.jd@gmail.com>
2023-01-31 15:40:43 +0100

    Working version

--- main.py
+++ main.py
@@ -1,27 +1,49 @@
 import cv2
-import tensorflow as tf
+import keras
+import numpy as np
 
-# model = cv2.dnn.readNetFromCaffe("face_recogn.h5")
+model = keras.models.load_model("face_recogn.h5")
 
-
-net = cv2.dnn.readNetFromTensorflow("face_recogn_pb/saved_model.pb", "face_recogn_pb/variables/variables.index")
+face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
 cap = cv2.VideoCapture(0)
-cap.set(4,480)
-cap.set(3,640)
+
 while True:
     ret, frame = cap.read()
-    if not ret:
-        break
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
 
-    # Przetwarzanie obrazu z kamerki
-    blob = cv2.dnn.blobFromImage(frame, 1.0, (300, 300), (104.0, 177.0, 123.0))
-    net.setInput(blob)
-    output = net.forward()
+    # Detect faces
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
 
-    # Wyświetlenie wyniku na ekranie
-    cv2.imshow("Frame", frame)
+    for (x, y, w, h) in faces:
+        # Only consider faces within the specified square
+        if x > 100 and y > 100 and x + w < 500 and y + h < 500:
+            # Crop the face and resize it
+            face_cropped = frame[y:y + h, x:x + w]
+            face_cropped = cv2.flip(face_cropped, 1)
+
+            face_resized = cv2.resize(face_cropped, (48, 48))
+
+
+            # Predict emotions
+            class_names = {'angry': 0, 'disgust': 1, 'fear': 2, 'happy': 3, 'neutral': 4, 'sad': 5, 'surprised': 6,}
+            predictions = model.predict(np.expand_dims(face_resized, axis=0))
+            prediction = predictions[0]
+            label = np.argmax(prediction)
+            class_value = list(class_names.keys())[label]
+
+            # Display the results
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+            font = cv2.FONT_HERSHEY_SIMPLEX
+            text = str(class_value)
+            cv2.putText(frame, text, (x, y), font, 1, (0, 0, 255), 2, cv2.LINE_AA)
+
+    
+    # Display the resulting frame
+    cv2.imshow("Emotion detection", frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+
 cap.release()
 cv2.destroyAllWindows()
--- model.py
+++ model.py
@@ -4,6 +4,7 @@ from keras import datasets, layers, models
 from keras.preprocessing.image import ImageDataGenerator
 
 import matplotlib.pyplot as plt
+from keras import backend as K
 
 train_images_path = "images/train"
 test_images_path = "images/validation"
@@ -17,12 +18,13 @@ train_data_prep = ImageDataGenerator(rescale=1./255,
     vertical_flip=True,
     )
 
-
+class_names = {'angry': 0, 'disgust': 1, 'fear': 2, 'happy': 3, 'neutral': 4, 'sad': 5, 'surprise': 6,}
 
 data = test_data_prep.flow_from_directory(train_images_path,
 target_size=(48, 48),
 batch_size=32,
 class_mode='categorical',
+classes=list(class_names.keys()),
 shuffle=True,
 seed=42
 )
@@ -30,6 +32,7 @@ val_data = train_data_prep.flow_from_directory(test_images_path,
 target_size=(48, 48),
 batch_size=32,
 class_mode='categorical',
+classes=list(class_names.keys()),
 shuffle=True,
 seed=42
 )
@@ -42,7 +45,6 @@ model.add(layers.MaxPooling2D(pool_size=(2, 2)))
 model.add(layers.Conv2D(64, kernel_size=(3, 3), activation='relu'))
 model.add(layers.Flatten())
 model.add(layers.Dense(64, activation='relu'))
-model.add(layers.Dropout(0.5))
 model.add(layers.Dense(7, activation="softmax"))
 
 model.compile(optimizer='adam',
@@ -50,16 +52,11 @@ model.compile(optimizer='adam',
               metrics=['accuracy'])
 
 
-model.fit(data, epochs=10, validation_data=val_data)
+model.fit(data, epochs=20, validation_data=val_data)
 
 model.summary()
 
 test_loss, test_acc = model.evaluate(val_data, verbose=2)
 print(test_acc)
 
-model.save("face_recogn.h5")
-sess = tf.compat.v1.keras.backend.get_session()
-graph_def = sess.graph.as_graph_def()
-
-frozen_graph = tf.compat.v1.graph_util.convert_variables_to_constants(sess, graph_def, ["dense_1"])
-tf.io.write_graph(frozen_graph, "face_recogn", "model.pb", as_text=False)
+model.save("face_recogn")
